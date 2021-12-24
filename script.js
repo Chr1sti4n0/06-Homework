6f8096a068095a8efd3c3d97c4289748fb374d35
Chris Jimenez <chrisjimenez8118@gmail.com>
2021-12-23 21:35:27 -0700

    Seventh Commit: Added 5 day forecast.

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ function latlonApi() {
         console.log(weatherData);
         displayData(weatherData);
         displayForecast1(weatherData);
+        displayForecast2(weatherData);
+        displayForecast3(weatherData);
+        displayForecast4(weatherData);
+        displayForecast5(weatherData);
     } 
     )
     //Set new weather array into local storage
@@ -99,5 +103,117 @@ function displayForecast1(data) {
 
         //Append display-weather to temperature
         $("#display-forecast1").append(mainCard1);
+        console.log(data);
+}
+
+function displayForecast2(data) {
+    $("#display-forecast2").empty();
+        
+    var mainCard2 = document.createElement("div");
+
+    var currentDate2 = document.createElement("h3");
+
+    currentDate2.textContent = moment().add(2, 'days').format('L');
+
+    var temperature2 = document.createElement("p");
+
+    temperature2.textContent = "Temp: " + data.daily[1].temp.day;
+
+    var windSpeed2 = document.createElement("p");
+
+    windSpeed2.textContent = "Wind Speed: " + data.daily[1].wind_speed;
+
+    var humidity2 = document.createElement("p");
+
+    humidity2.textContent = "Humidity: " + data.daily[1].humidity;
+
+    mainCard2.append(currentDate2, temperature2, windSpeed2, humidity2);
+
+        //Append display-weather to temperature
+        $("#display-forecast2").append(mainCard2);
+        console.log(data);
+}
+
+function displayForecast3(data) {
+    $("#display-forecast3").empty();
+        
+    var mainCard3 = document.createElement("div");
+
+    var currentDate3 = document.createElement("h3");
+
+    currentDate3.textContent = moment().add(3, 'days').format('L');
+
+    var temperature3 = document.createElement("p");
+
+    temperature3.textContent = "Temp: " + data.daily[2].temp.day;
+
+    var windSpeed3 = document.createElement("p");
+
+    windSpeed3.textContent = "Wind Speed: " + data.daily[2].wind_speed;
+
+    var humidity3 = document.createElement("p");
+
+    humidity3.textContent = "Humidity: " + data.daily[2].humidity;
+
+    mainCard3.append(currentDate3, temperature3, windSpeed3, humidity3);
+
+        //Append display-weather to temperature
+        $("#display-forecast3").append(mainCard3);
+        console.log(data);
+}
+
+function displayForecast4(data) {
+    $("#display-forecast4").empty();
+        
+    var mainCard4 = document.createElement("div");
+
+    var currentDate4 = document.createElement("h3");
+
+    currentDate4.textContent = moment().add(4, 'days').format('L');
+
+    var temperature4 = document.createElement("p");
+
+    temperature4.textContent = "Temp: " + data.daily[3].temp.day;
+
+    var windSpeed4 = document.createElement("p");
+
+    windSpeed4.textContent = "Wind Speed: " + data.daily[3].wind_speed;
+
+    var humidity4 = document.createElement("p");
+
+    humidity4.textContent = "Humidity: " + data.daily[3].humidity;
+
+    mainCard4.append(currentDate4, temperature4, windSpeed4, humidity4);
+
+        //Append display-weather to temperature
+        $("#display-forecast4").append(mainCard4);
+        console.log(data);
+}
+
+function displayForecast5(data) {
+    $("#display-forecast5").empty();
+        
+    var mainCard5 = document.createElement("div");
+
+    var currentDate5 = document.createElement("h3");
+
+    currentDate5.textContent = moment().add(5, 'days').format('L');
+
+    var temperature5 = document.createElement("p");
+
+    temperature5.textContent = "Temp: " + data.daily[4].temp.day;
+
+    var windSpeed5 = document.createElement("p");
+
+    windSpeed5.textContent = "Wind Speed: " + data.daily[4].wind_speed;
+
+    var humidity5 = document.createElement("p");
+
+    humidity5.textContent = "Humidity: " + data.daily[4].humidity;
+
+    mainCard5.append(currentDate5, temperature5, windSpeed5, humidity5);
+
+        //Append display-weather to temperature
+        $("#display-forecast5").append(mainCard5);
         console.log(data);
 }
